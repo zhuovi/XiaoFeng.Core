7adadb603ec8153b55ff6583207ced443f548687
zhuovi <jacky@fayelf.com>
2023-05-30 08:39:39 +0800

    删除过渡命名空间XiaoFeng.Model.Core;优化通过模型生成数据表;新增索引属性TableIndexAttribute;新增模型索引属性;新增获取模型索引属性;新增查找数据库表是否存在;修复获取枚举GetDescription时无当前枚举时报错;增加调度作业Ijob中参数可通过方法分步设置;设置作业任务接口IJobWorker;

---
+++ Threading/IJobWoker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+/****************************************************************
+*  Copyright © (2023) www.fayelf.com All Rights Reserved.       *
+*  Author : jacky                                               *
+*  QQ : 7092734                                                 *
+*  Email : jacky@fayelf.com                                     *
+*  Site : www.fayelf.com                                        *
+*  Create Time : 2023-05-26 17:39:04                            *
+*  Version : v 1.0.0                                            *
+*  CLR Version : 4.0.30319.42000                                *
+*****************************************************************/
+namespace XiaoFeng.Threading
+{
+    /// <summary>
+    /// 调度作业
+    /// </summary>
+    public interface IJobWoker
+    {
+        /// <summary>
+        /// 作业任务
+        /// </summary>
+        /// <returns></returns>
+        Task Invoke();
+    }
+}
